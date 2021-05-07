79f9f30ec7c2ea86e7d43b176f8da826649adea7
Shinan Liao <shinanliao1@gmail.com>
2021-05-07 15:04:49 -0700

    adding url param for cap word

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get "/phrase", controller: "params", action: "phrase"
+  #?word=
+get "/phrase/:word", controller: "params", action: "phrase"
+  #phrase/#
 end
